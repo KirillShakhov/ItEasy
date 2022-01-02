b7abaa7afbba9fba757d6bcac6a421a884985b57
kirill <kirill.shakhov14@gmail.com>
2022-01-03 06:04:11 +0700

    add hidden keyboard

--- ItEasy/Auth/RegisterView.swift
+++ ItEasy/Auth/RegisterView.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class RegisterView: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var registerButton: UIButton!
 	@IBOutlet weak var slideIdicator: UIView!
 
     var hasSetPointOrigin = false
     var pointOrigin: CGPoint?
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +24,11 @@ class RegisterView: UIViewController {
 		
 		let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 	   view.addGestureRecognizer(tap)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+	
 	}
     
     override func viewDidLayoutSubviews() {
@@ -39,13 +44,13 @@ class RegisterView: UIViewController {
 	}
 	
     @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: view)
+		let windowTranslation = sender.translation(in: view)
         
         // Not allowing the user to drag the view upward
-        guard translation.y >= 0 else { return }
+        guard windowTranslation.y >= 0 else { return }
         
         // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
-        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
+        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + windowTranslation.y)
         
         if sender.state == .ended {
             let dragVelocity = sender.velocity(in: view)
@@ -58,5 +63,19 @@ class RegisterView: UIViewController {
                 }
             }
         }
+		dismissKeyboard()
     }
+	
+	
+	
+	@objc func keyboardWillShow(notification: NSNotification) {
+		  // move the root view up by the distance of keyboard height
+		  self.view.frame.origin.y = 100
+	  }
+
+	@objc func keyboardWillHide(notification: NSNotification) {
+
+	  // move back the root view origin to zero
+		self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 700)
+	}
 }
