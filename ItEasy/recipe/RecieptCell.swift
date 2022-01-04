127f4dc82ed2b21b809b6465ca4f49cba0481af1
kirill <kirill.shakhov14@gmail.com>
2022-01-05 06:28:16 +0700

    fix RecipeCell shadow

--- ItEasy/recipe/RecieptCell.swift
+++ ItEasy/recipe/RecieptCell.swift
@@ -12,4 +12,12 @@ class RecieptCell: UICollectionViewCell {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var kcal: UILabel!
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		self.layer.masksToBounds = true
+		self.layer.shadowOffset = CGSize(width: 0, height: 0)
+		self.layer.shadowRadius = 0
+		self.layer.shadowOpacity = 0
+	}
 }
--- ItEasy/util/ShadowView.swift
+++ ItEasy/util/ShadowView.swift
@@ -10,12 +10,16 @@ import UIKit
 @IBDesignable
 class ShadowView: UIView {
 
+	@IBInspectable var cornerRadius: CGFloat = 20
+	@IBInspectable var shadowRadius: CGFloat = 5.0
+
 	override func layoutSubviews() {
 		super.layoutSubviews()
+		self.layer.cornerRadius = cornerRadius
 		self.layer.masksToBounds = false
 		self.layer.shadowColor = UIColor.gray.cgColor
 		self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-		self.layer.shadowRadius = 5.0
+		self.layer.shadowRadius = shadowRadius
 		self.layer.shadowOpacity = 1.0
 		self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.layer.cornerRadius).cgPath
 	}
