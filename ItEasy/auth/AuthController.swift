b1675a4bcd65a0c8c6f1d2eb9df47c86dcc3050a
kirill <kirill.shakhov14@gmail.com>
2022-01-06 04:32:38 +0700

    add loading view

--- ItEasy/auth/AuthController.swift
+++ ItEasy/auth/AuthController.swift
@@ -37,39 +37,6 @@ class AuthController: UIViewController {
 		
 	}
 	
-	override func viewDidAppear(_ animated: Bool) {
-		let defaults = UserDefaults.standard
-		if let username = defaults.string(forKey: "username") {
-			if let pass = defaults.string(forKey: "pass"){
-			let result = Sender.query(address: "http://127.0.0.1:8090/auth?login="+username+"&pass="+pass)
-			let json = result.data(using: .utf8)!
-			let decoder = JSONDecoder()
-			struct Request: Codable {
-				var status: String
-				var message: String?
-			}
-			do{
-				let product = try decoder.decode(Request.self, from: json)
-				if(product.status == "ok") {
-					let storyboard = UIStoryboard(name: "Main", bundle: nil)
-					let vc = storyboard.instantiateViewController(withIdentifier: "MainViewController") as UIViewController
-					vc.modalPresentationStyle = .fullScreen
-					dismiss(animated: false)
-					present(vc, animated: false, completion: nil)
-				}
-				else{
-					defaults.removeObject(forKey: "username")
-					defaults.removeObject(forKey: "pass")
-				}
-			}
-				catch{
-					print("Неожиданная ошибка: \(error).")
-					return
-				}
-			}
-		}
-	}
-	
 	
     
 //	func goToNextPage(){
---
+++ ItEasy/auth/LoadingAuthViewController.swift
@@ -0,0 +1,70 @@
+//
+//  LoadingAuthViewController.swift
+//  ItEasy
+//
+//  Created by Кирилл Шахов on 06.01.2022.
+//
+
+import UIKit
+
+class LoadingAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	override func viewDidAppear(_ animated: Bool) {
+		let defaults = UserDefaults.standard
+		if let username = defaults.string(forKey: "username") {
+			if let pass = defaults.string(forKey: "pass"){
+			let result = Sender.query(address: "http://127.0.0.1:8090/auth?login="+username+"&pass="+pass)
+			let json = result.data(using: .utf8)!
+			let decoder = JSONDecoder()
+			struct Request: Codable {
+				var status: String
+				var message: String?
+			}
+			do{
+				let product = try decoder.decode(Request.self, from: json)
+				if(product.status == "ok") {
+					goToView(name: "Main", withIdentifier: "MainViewController")
+				}
+				else{
+					defaults.removeObject(forKey: "username")
+					defaults.removeObject(forKey: "pass")
+					goToView(name: "Auth", withIdentifier: "Auth")
+				}
+			}
+				catch{
+					print("Неожиданная ошибка: \(error).")
+					return
+				}
+			}
+		}
+		else{
+			goToView(name: "Auth", withIdentifier: "Auth")
+		}
+	}
+	
+	func goToView(name: String, withIdentifier: String){
+		let storyboard = UIStoryboard(name: name, bundle: nil)
+		let vc = storyboard.instantiateViewController(withIdentifier: withIdentifier) as UIViewController
+		vc.modalPresentationStyle = .fullScreen
+		dismiss(animated: false)
+		present(vc, animated: true, completion: nil)
+	}
+	
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
